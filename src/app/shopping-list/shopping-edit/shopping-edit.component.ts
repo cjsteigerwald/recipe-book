7466a345339c896aca80edc0edec6f2c0a2d9c61
chris steigerwald <cjsteigerwald@gmail.com>
2020-06-03 03:41:20 -0700

    Created new project to clean up some errors. So... moved new project and making initial commit with it, next time should have branched.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput', {static: false})
+  nameInputRef: ElementRef;
+  
+  @ViewChild('amountInput', {static: false})
+  amountInputRef: ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem(){
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmout = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmout);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
